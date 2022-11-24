ce8c7f7a696db242caf672216c25361369e18c0c
terria1020 <jaehan1346@gmail.com>
2022-11-24 23:23:24 +0900

    Swagger 적용
    Swagger 적용을 위한 implementation, SwaggerConfig 생성 및 application.properties에 pathmatch 관련 추가

--- src/main/java/com/project/db/mju/webserver/config/SwaggerConfig.java
+++ src/main/java/com/project/db/mju/webserver/config/SwaggerConfig.java
@@ -1,6 +1,32 @@
 package com.project.db.mju.webserver.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 @Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(SwaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.db.mju.webserver.web.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo SwaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("디비설 프로젝트 Swagger")
+                .description("디비설 프로젝트 API Swagger 문서")
+                .version("1.0")
+                .build();
+    }
 }
--- src/main/java/com/project/db/mju/webserver/web/controller/HomeController.java
+++ src/main/java/com/project/db/mju/webserver/web/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.project.db.mju.webserver.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
